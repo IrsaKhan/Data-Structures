2a95519e868c2bd229fafe7603dfbb52d52ff302
IrsaKhan <irsakhan240@gmail.com>
2021-08-31 18:35:59 +0500

    Bakward Linked List DS

---
+++ BakwardLL.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+struct nodeType
+{
+int info;
+nodeType *link;
+};
+
+nodeType* buildListBackward()
+{
+nodeType *first, *newNode;
+int num;
+cout << "Enter a list of integers ending with -999."
+<< endl;
+cin >> num;
+first = NULL;
+while (num != -999)
+{
+newNode = new nodeType; //create a node
+newNode->info = num; //store the data in newNode
+newNode->link = first; //put newNode at the beginning
+//of the list
+first = newNode; //update the head pointer of
+//the list, that is, first
+cin >> num; //read the next number
+}
+return first;
+} //end buildListBackward
